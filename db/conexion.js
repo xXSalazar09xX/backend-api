dd5fb987427d31050721ad968f7f7518ce977ee4
Cristofer Leonardo Salazar Palacios <crisxxsalazar15@gmail.com>
2024-07-30 09:12:11 -0500

    DB_PORT

--- db/conexion.js
+++ db/conexion.js
@@ -1,12 +1,13 @@
 import { Sequelize } from 'sequelize';
-import {DB_CONNECTION, DB_USERNAME, DB_PASSWORD,DB_HOST,DB_DATABASE} from '../config/config.js';
+import {DB_CONNECTION, DB_USERNAME, DB_PASSWORD,DB_HOST,DB_DATABASE, DB_PORT} from '../config/config.js';
 export const sequelize = new Sequelize(
     DB_DATABASE,
     DB_USERNAME,
     DB_PASSWORD,
     {
         host: DB_HOST,
-        dialect: DB_CONNECTION
+        dialect: DB_CONNECTION,
+        port: DB_PORT
     }
 );
 
